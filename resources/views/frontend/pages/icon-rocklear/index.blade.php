2f60d7dcf0273ce1ea090336f5db58a39c2274a2
Zain ul Abdeen <xainjatt@gmail.com>
2024-02-06 01:01:23 +0500

    addded rocklear page

--- resources/views/frontend/pages/icon-rocklear/index.blade.php
+++ resources/views/frontend/pages/icon-rocklear/index.blade.php
@@ -20,7 +20,7 @@
 
     <link rel="stylesheet" href="{{ asset('css/ep-marquee.css') }}" />
 
-    <link rel="stylesheet" href="{{ asset('icon_rocklear/css/style.css?v=11') }}" />
+    <link rel="stylesheet" href="{{ asset('icon_rocklear/css/style.css?v=11asdf') }}" />
 @endsection
 
 @section('content')
