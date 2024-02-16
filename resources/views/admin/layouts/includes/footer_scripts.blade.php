40658208f9178a8784e6b5cd22ca01e22dd93d05
Zain ul Abdeen <xainjatt@gmail.com>
2024-02-16 19:58:21 +0500

    added quill js

--- resources/views/admin/layouts/includes/footer_scripts.blade.php
+++ resources/views/admin/layouts/includes/footer_scripts.blade.php
@@ -34,10 +34,66 @@
         $("#email").select2();
     });
 
+    const toolbarOptions = [
+        ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+        ['blockquote', 'code-block'],
+        ['link', 'image', 'video', 'formula'],
+
+        [{
+            'header': 1
+        }, {
+            'header': 2
+        }], // custom button values
+        [{
+            'list': 'ordered'
+        }, {
+            'list': 'bullet'
+        }, {
+            'list': 'check'
+        }],
+        [{
+            'script': 'sub'
+        }, {
+            'script': 'super'
+        }], // superscript/subscript
+        [{
+            'indent': '-1'
+        }, {
+            'indent': '+1'
+        }], // outdent/indent
+        [{
+            'direction': 'rtl'
+        }], // text direction
+
+        [{
+            'size': ['small', false, 'large', 'huge']
+        }], // custom dropdown
+        [{
+            'header': [1, 2, 3, 4, 5, 6, false]
+        }],
+
+        [{
+            'color': []
+        }, {
+            'background': []
+        }], // dropdown with defaults from theme
+        [{
+            'font': []
+        }],
+        [{
+            'align': []
+        }],
+
+        ['clean'] // remove formatting button
+    ];
+
 
     var quill = new Quill('textarea#description', {
-    theme: 'snow'
-  });
+        modules: {
+            toolbar: toolbarOptions
+        },
+        theme: 'snow'
+    });
 
     // var useDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
     // tinymce.init({
