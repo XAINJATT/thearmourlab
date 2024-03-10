8346d26081bb9c3a640f773cd9889dae91637464
Zain ul Abdeen <xainjatt@gmail.com>
2024-03-10 12:38:42 +0500

    fixed image comparer

--- resources/views/frontend/pages/icon-rocklear/index.blade.php
+++ resources/views/frontend/pages/icon-rocklear/index.blade.php
@@ -11,15 +11,15 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet" />
-    <link rel="stylesheet" href="https://unpkg.com/image-compare-viewer/dist/image-compare-viewer.min.css" />
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-    <!-- Swiper JS -->
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-
-    <link rel="stylesheet" href="{{ asset('css/ep-marquee.css') . config('app.asset') }}" />
-
-    <link rel="stylesheet" href="{{ asset('icon_rocklear/css/style.css') . config('app.asset') }}" />
+        
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+        <!-- Swiper JS -->
+        <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+        
+        <link rel="stylesheet" href="{{ asset('css/ep-marquee.css') . config('app.asset') }}" />
+        
+        <link rel="stylesheet" href="{{ asset('icon_rocklear/css/style.css') . config('app.asset') }}" />
+        <link rel="stylesheet" href="https://unpkg.com/image-compare-viewer/dist/image-compare-viewer.min.css" />
 @endsection
 
 @section('content')
