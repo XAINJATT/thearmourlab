9b7b27f491583611d4161bab66fecb46e94afece
Zain ul Abdeen <xainjatt@gmail.com>
2024-03-27 23:57:24 +0500

    fixed moving posts rename

--- resources/views/frontend/pages/icon-rocklear/index.blade.php
+++ resources/views/frontend/pages/icon-rocklear/index.blade.php
@@ -1,6 +1,6 @@
-{{-- @extends('frontend.layout.app') --}}
+@extends('frontend.layout.app')
 
-{{-- @section('title')
+@section('title')
     {{ __('THE ARMOUR LAB') }}
 @endsection
 
@@ -25,25 +25,7 @@
     href="https://iconrocklear.com/wp-content/plugins/bdthemes-element-pack/assets/css/ep-image-compare.css?ver=7.1.0">
 @endsection
 
-@section('content') --}}
-<title>Icon Rock Lear</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet" />
-
-    <link rel="stylesheet" href="https://unpkg.com/image-compare-viewer/dist/image-compare-viewer.min.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-    <!-- Swiper JS -->
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    
-    <link rel="stylesheet" href="{{ asset('css/ep-marquee.css') . config('app.asset') }}" />
-    
-    <link rel="stylesheet" href="{{ asset('icon_rocklear/css/style.css') . config('app.asset') }}" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet"
-    href="https://iconrocklear.com/wp-content/plugins/bdthemes-element-pack/assets/css/ep-image-compare.css?ver=7.1.0">
+@section('content')
     <main>
         <!--
                                           |--------------------------------------------------------------------------
@@ -1663,9 +1645,9 @@
             </div>
         </section>
     </main>
-{{-- @endsection --}}
+@endsection
 
-{{-- @section('scripts') --}}
+@section('scripts')
     <script src="js/ep-marquee.min.js"></script>
 
     <script>
@@ -2007,7 +1989,7 @@
         ];
     </script>
 
-    {{-- <script src="{{ asset('js/script.js') . config('app.asset') }}"></script> --}}
+    <script src="{{ asset('js/script.js') . config('app.asset') }}"></script>
     <script src="https://unpkg.com/image-compare-viewer@1.6.2/dist/image-compare-viewer.min.js"></script>
 
 
@@ -2056,4 +2038,4 @@
             // });
         });
     </script>
-{{-- @endsection --}}
+@endsection
