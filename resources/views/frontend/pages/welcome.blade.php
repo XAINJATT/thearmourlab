47ccdcec99fe9f86e9b6d58a2eeb30f395bde89c
Zain ul Abdeen <xainjatt@gmail.com>
2024-05-10 17:43:08 +0500

    added new section

--- resources/views/frontend/pages/welcome.blade.php
+++ resources/views/frontend/pages/welcome.blade.php
@@ -68,7 +68,7 @@
                                 technology to apply premium Paint Protection Films and Ceramic Coatings, ensuring your car
                                 doesn't just shine but is also safeguarded against scratches, dirt, and UV rays.</p>
                         </div>
-                        <a href="{{route('frontend.about')}}" class="btn btn-primary">About Us</a>
+                        <a href="{{ route('frontend.about') }}" class="btn btn-primary">About Us</a>
                     </div>
                     <div class="col-lg-7 mb-30">
                         <div class="about-media">
@@ -91,30 +91,191 @@
             </div>
         </section>
 
+
+        <style>
+            /*
+            |--------------------------------------------------------------------------
+            | Section 14
+            |--------------------------------------------------------------------------
+            */
+
+            .section-14 {
+                background-image: url(../images/bg0s.png);
+                background-position: center center;
+                background-repeat: no-repeat;
+                background-size: cover;
+                position: relative;
+                transition: background 0.3s, border 0.3s, border-radius 0.3s,
+                    box-shadow 0.3s;
+                padding-top: 70px;
+                padding-bottom: 100px;
+            }
+
+            .section-14-bg {
+                background-image: url(https://iconrocklear.com/wp-content/uploads/2023/08/Screenshot-at-Aug-27-16-17-58.png);
+                background-position: top center;
+                background-repeat: no-repeat;
+                background-size: cover;
+                transition: background 0.3s, border 0.3s, border-radius 0.3s,
+                    box-shadow 0.3s;
+                margin-top: -100px;
+                margin-bottom: 0px;
+                height: 84px;
+                margin-top: 100px;
+            }
+
+            .section-14 .section-container {
+                position: relative;
+            }
+
+            .section-14 h4 {
+                font-family: "Montserrat", Sans-serif;
+                font-size: 38px;
+                font-weight: 800;
+                text-transform: uppercase;
+                fill: #ffffff;
+                color: #ffffff;
+                background-color: transparent;
+                background-image: linear-gradient(90deg,
+                        var(--e-global-color-primary) 3%,
+                        var(--e-global-color-accent) 0%);
+                border-style: solid;
+                border-width: 1px 1px 1px 0px;
+                border-color: #02010100;
+                border-radius: 0px 0px 0px 0px;
+                padding: 8px 18px 8px 12px;
+                position: absolute;
+                /* margin-bottom: 27px; */
+                top: -43px;
+                display: inline-block;
+                z-index: 1;
+            }
+
+            .openable-text {
+                padding: 10px 100px 10px 10px;
+                border-top: 1px solid #d5d8dc;
+                color: var(--e-global-color-text);
+                font-size: 16px;
+                display: none;
+            }
+
+            .open-text {
+                font-weight: 700;
+                cursor: pointer;
+            }
+
+            .section-14 .image-are {
+                height: auto;
+                max-width: 100%;
+                border: none;
+                border-radius: 0;
+                box-shadow: none;
+            }
+
+            .header-title-small {
+                color: #262626;
+                font-family: "Montserrat", Sans-serif;
+                font-size: 20px;
+                font-weight: 900;
+                margin-bottom: 10px;
+                line-height: 1.4em;
+            }
+
+            .header-title {
+                color: #262626;
+                font-family: "Montserrat", Sans-serif;
+                font-size: 96px;
+                font-weight: 800;
+                text-transform: uppercase;
+                margin-bottom: 40px;
+            }
+
+            .customTab-custom .customTab.cta-primary {
+                text-shadow: none;
+                background-color: inherit;
+            }
+
+            .customTab-custom .customTab.cta-primary:hover {
+                fill: #ffffff;
+                color: #ffffff;
+                background-color: transparent;
+                background-image: linear-gradient(90deg,
+                        var(--e-global-color-primary) 2%,
+                        var(--e-global-color-accent) 0%);
+            }
+        </style>
+
+        <div class="section-14-bg"></div>
+        <section class="section-14 ">
+            <div class="section-container container">
+                <h4 class="header-title-small">ICON</h4>
+                <hr class="hr-light" />
+                <h2 class="header-title text-white">ROCKKLEAR</h2>
+                <div class="row">
+                    <div class="col-md-12 col-lg-6 col-sm-12">
+                        <h6 class="open-text text-white">
+                            Unlock the Future of Surface Protection with Icon Rocklear
+                        </h6>
+                        <div style="display:block;" class="openable-text">
+                            <p>
+                                Experience unparalleled durability with Icon Rocklear, the ultimate surface protection
+                                technology. Designed for automotive, marine, and aviation, Icon Rocklear is 75x stronger and
+                                35x thicker than other coatings. With zero maintenance and a lifetime warranty, ensure your
+                                investment remains pristine.
+                            </p>
+
+                        </div>
+
+                        <div class="cta-btns mt-5 mb-5">
+                            {{-- <a href="{{ route('frontend.contact') }}" rel="noopener noreferrer">
+                                <button class="cta-primary">INQUIRIES</button>
+                            </a> --}}
+                            <a href="{{ route("icon-rocklear.index") }}" rel="noopener noreferrer">
+                                <button class="cta-primary">LEARN MORE</button>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-12 col-lg-6 col-sm-12">
+                        <div class="image-are">
+                            <img loading="lazy" decoding="async" width="800" height="446" src="images/Group-26-5.png"
+                                class="img-fluid" alt=""
+                                srcset="
+                    images/Group-26-5.png         818w,
+                    images/Group-26-5-300x167.png 300w,
+                    images/Group-26-5-768x428.png 768w
+                  "
+                                sizes="(max-width: 800px) 100vw, 800px" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
         <!-- Services -->
-        @include("frontend.partials.home-services")
-        
+        @include('frontend.partials.home-services')
+
 
         <!-- Why Choose us -->
-       
-        @include("frontend.partials.why-choose-us")
-        
+
+        @include('frontend.partials.why-choose-us')
+
         <!-- Portfolio -->
-        @include("frontend.partials.home-gallery")
-        
-        
+        @include('frontend.partials.home-gallery')
+
+
         <!-- Appointment Form -->
-        @include("frontend.partials.home-contact")
-        
-        
-        
+        @include('frontend.partials.home-contact')
+
+
+
         <!-- Testimonial -->
         @include('frontend.partials.testemonials')
-        
-        
+
+
         <!-- Blog -->
-        @include("frontend.partials.home-blogs", ['blogDetails' => $blogDetails])
-      
+        @include('frontend.partials.home-blogs', ['blogDetails' => $blogDetails])
+
 
     </div>
 @endsection
