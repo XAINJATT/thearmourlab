d28f49c49a97217ceb0302e79e419197c588c8cb
Zain ul Abdeen <xainjatt@gmail.com>
2024-07-03 18:19:43 +0500

    fixed blogs

--- resources/views/frontend/partials/meta.blade.php
+++ resources/views/frontend/partials/meta.blade.php
@@ -112,3 +112,14 @@
     &noscript=1"/>
     </noscript>
     <!-- End Meta Pixel Code -->
+
+
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-E1B34Y35YL"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-E1B34Y35YL');
+</script>
